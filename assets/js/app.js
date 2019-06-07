b5c0bf0bad8aa7e50eec6b43b5e4910c9b73baab
GrimmOutlook <grimmoutlook87@gmail.com>
2019-06-07 12:43:29 -0400

    Set up minimal testing.  Added NavBar and home screen.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,9 @@ import css from '../css/app.css';
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+window.setTimeout(function() {
+    var x = document.getElementsByClassName("alert");
+     x[0].style.display = 'none';
+     x[1].style.display = 'none';
+ }, 3000);
